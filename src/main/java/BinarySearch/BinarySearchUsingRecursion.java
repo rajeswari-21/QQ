c1206ba7000ee18dd1d4741c4589bde3d6b33434
rajeswari-21 <rajeswarimallick21@gmail.com>
2024-11-10 08:44:16 +0530

    df sun

--- src/main/java/BinarySearch/BinarySearchUsingRecursion.java
+++ src/main/java/BinarySearch/BinarySearchUsingRecursion.java
@@ -1,4 +1,31 @@
 package BinarySearch;
 
 public class BinarySearchUsingRecursion {
-}
+
+    static int search(int[] arr, int target)
+    {
+        return bs(arr,0,arr.length-1,target);
+    }
+
+    static int bs(int[] arr,int start, int end,int target)
+    {
+        if(start>end)
+            return -1;
+        int mid= (start+end)/2;
+        if(arr[mid]==target)
+            return mid;
+        else if (arr[mid]<target)
+            return bs(arr,mid+1,end,target);
+        else
+            return bs(arr,start,mid-1,target);
+
+        }
+
+        public static void main(String[] st)
+        {
+            int i[]= new int[]{-1,0,3,5,9,12};
+            //int i[]= new int[]{1};
+            System.out.println(search(i,9));
+        }
+    }
+
